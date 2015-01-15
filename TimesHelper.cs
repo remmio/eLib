17138bdeb4361d32295bddec4c9a97fb34410d3e
Halidou Cisse <halidoucse@gmail.com>
2015-01-15 21:47:05 +0000

    _

---
+++ TimesHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace CLib
+{
+    /// <summary>
+    /// Date and Times Helper
+    /// </summary>
+    public class TimesHelper
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ts"></param>
+        /// <returns></returns>
+        public static string ProperTimeSpan(TimeSpan ts)
+        {
+            var time = string.Format("{0:00}", ts.Minutes);   //:{1:00}
+            var hours = (int)ts.TotalHours;
+            if (hours > 0) time = hours + ":" + time;
+            return time;
+        }
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
