f939b8d9cfcd49b0577ddb136f2aa0b5a95b2ce0
Halid Cisse <halidoucse@gmail.com>
2015-03-28 23:45:10 +0000

    _

--- Validation/InputHelper.cs
+++ Validation/InputHelper.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Mail;
+﻿using System.Globalization;
+using System.Net.Mail;
 using System.Text.RegularExpressions;
 using libphonenumber;
 
@@ -8,7 +9,8 @@ namespace CLib.Validation {
     /// <summary>
     /// 
     /// </summary>
-    public static class InputHelper {
+    public static class InputHelper
+    {
 
         /// <summary>
         /// Verifie si c'est un numero de telephoone valid
@@ -16,12 +18,17 @@ namespace CLib.Validation {
         /// <param name="phoneNumber"></param>
         /// <param name="contryCode"></param>
         /// <returns></returns>
-        public static bool IsValidNumber (string phoneNumber, string contryCode = "MA")
-        {                     
-            try {
+        public static bool IsValidNumber(string phoneNumber, string contryCode = "MA")
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(contryCode))
+                    contryCode = RegionInfo.CurrentRegion.TwoLetterISORegionName;
+
                 var numberProto = PhoneNumberUtil.Instance.Parse(phoneNumber, contryCode);
-                return numberProto.IsValidNumber ;
-            } catch (NumberParseException)
+                return numberProto.IsValidNumber;
+            }
+            catch (NumberParseException)
             {
                 return false;
             }
@@ -35,10 +42,18 @@ namespace CLib.Validation {
         /// <returns></returns>
         public static string FormatNumber(string phoneNumber, string contryCode = "MA")
         {
-            try {
+            try
+            {
+                if (string.IsNullOrEmpty(contryCode))
+                    contryCode=RegionInfo.CurrentRegion.TwoLetterISORegionName;
+
                 var numberProto = PhoneNumberUtil.Instance.Parse(phoneNumber, contryCode);
-                return numberProto.IsValidNumber ? numberProto.Format(PhoneNumberUtil.PhoneNumberFormat.NATIONAL) : phoneNumber;
-            } catch (NumberParseException) {
+                return numberProto.IsValidNumber
+                    ? numberProto.Format(PhoneNumberUtil.PhoneNumberFormat.INTERNATIONAL)
+                    : phoneNumber;
+            }
+            catch (NumberParseException)
+            {
                 return phoneNumber;
             }
         }
@@ -48,20 +63,29 @@ namespace CLib.Validation {
         /// </summary>
         /// <param name="email"></param>
         /// <returns></returns>
-        public static bool IsEmail (string email) {
-            if(string.IsNullOrEmpty(email)) { return false; }
-            try {
-                var regex = new Regex("^((([a-z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])"+
-                        "+(\\.([a-z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])+)*)|((\\x22)"+
-                        "((((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(([\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x7f]|\\x21|[\\x23-\\x5b]|[\\x5d-\\x7e]|"+
-                        "[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(\\\\([\\x01-\\x09\\x0b\\x0c\\x0d-\\x7f]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\u"+
-                        "FDF0-\\uFFEF]))))*(((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(\\x22)))@((([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|"+
-                        "(([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([a-z]|\\d|"+
-                        "[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))\\.)+(([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(([a-z]|[\\u00A0-\\uD7FF\\uF900"+
-                        "-\\uFDCF\\uFDF0-\\uFFEF])([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFF"+
-                        "EF])))\\.?$", RegexOptions.IgnoreCase|RegexOptions.ExplicitCapture|RegexOptions.Compiled);
+        public static bool IsEmail(string email)
+        {
+            if (string.IsNullOrEmpty(email))
+            {
+                return false;
+            }
+            try
+            {
+                var regex =
+                    new Regex(
+                        "^((([a-z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])" +
+                        "+(\\.([a-z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])+)*)|((\\x22)" +
+                        "((((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(([\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x7f]|\\x21|[\\x23-\\x5b]|[\\x5d-\\x7e]|" +
+                        "[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(\\\\([\\x01-\\x09\\x0b\\x0c\\x0d-\\x7f]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\u" +
+                        "FDF0-\\uFFEF]))))*(((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(\\x22)))@((([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|" +
+                        "(([a-z]|\\d|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([a-z]|\\d|" +
+                        "[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])))\\.)+(([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])|(([a-z]|[\\u00A0-\\uD7FF\\uF900" +
+                        "-\\uFDCF\\uFDF0-\\uFFEF])([a-z]|\\d|-|\\.|_|~|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFFEF])*([a-z]|[\\u00A0-\\uD7FF\\uF900-\\uFDCF\\uFDF0-\\uFF" +
+                        "EF])))\\.?$", RegexOptions.IgnoreCase | RegexOptions.ExplicitCapture | RegexOptions.Compiled);
                 return regex.IsMatch(email);
-            } catch (RegexMatchTimeoutException) {
+            }
+            catch (RegexMatchTimeoutException)
+            {
                 return false;
             }
         }
@@ -71,14 +95,26 @@ namespace CLib.Validation {
         /// </summary>
         /// <param name="email"></param>
         /// <returns></returns>
-        public static bool IsValidEmail (string email) {
-            try {
+        public static bool IsValidEmail(string email)
+        {
+            try
+            {
                 var addr = new MailAddress(email);
-                return addr.Address==email;
-            } catch {
+                return addr.Address == email;
+            }
+            catch
+            {
                 return false;
             }
         }
 
+        /// <summary>
+        /// GetCurrencySymbol
+        /// </summary>
+        /// <returns></returns>
+        public static string GetCurrencySymbol()
+        {
+            return RegionInfo.CurrentRegion.ISOCurrencySymbol;
+        }
     }
 }
